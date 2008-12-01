c3c58987cc5a618f6f9d67be4396d33172272941
Scott Dial <scott@scottdial.com>
2008-12-01 21:05:15 +0000

    Quoted inline code snippets. Changed operator @ to %.

--- docs/index.php
+++ docs/index.php
@@ -54,9 +54,9 @@ p { margin: 0; margin-left: 1em; margin-bottom: 1em; text-align: justify; }
 </head>
 <body style="">
 <div class="header" style="font-size: large">Parsing Expression Grammar Parsing Expression Generator (PEGPEG)</div>
-<div class="header" style="font-size: small">Scott A. Dial</div>
+<div class="header" style="font-size: small; padding-bottom: 1em">Scott A. Dial</div>
 
-<div class="section">1. PEG Parser: <?inline_code('(peg-parser [binding ...] nonterminal ...)')?></div>
+<div class="section">1. PEG Parser: <? inline_code('(peg-parser [binding ...] nonterminal ...)') ?></div>
 
 <div>The <? inline_code("(peg-parser)") ?> syntatic form produces a procedure
 of one argument, a thunk which is to yield the input one value at a time. The
@@ -79,104 +79,181 @@ the generator was rejected.</div>
 
 <div class="section">2.1. Nonterminals (symbols)</div>
 
+<p>
+Any symbol appearing in a PEG expression is treated as a nonterminal reference.
+A symbol's root prefix is matched against the bindings form to determine the
+nonterminal it represents. A root prefix can be any valid scheme identifier but
+must not end with any number of digits. Within a PEG expression, you may append
+any number of digits to an identifier declared in the bindings to obtain a
+unique identifier. The value of the semantic action applied for that
+nonterminal will be bound to that unique identifier.
+</p>
+
+<? show_code('(peg-parser
+  [(foo F)]
+  (S
+    [(foo0 foo1)
+     (list foo0 foo1)]
+  (F
+    ["foo" \'foo])))'); ?>
+
+<p>
+In the example above, both of the symbols "<? inline_code('foo0') ?>" and
+"<? inline_code('foo1') ?>" references the binding
+"<? inline_code('(foo F)') ?>", which binds all symbols of the form
+"<? inline_code('foo[0-9]+') ?>" to the nonterminal named
+"<? inline_code('F') ?>".
+</p>
+
 <div class="section">2.2. Terminals (values)</div>
 
 <p>
 Any scheme value can be matched as a value (a value is matched if the value
-from the input stream generator is <? inline_code('eqv?') ?> to the terminal),
-however the PEGPEG expression grammar has been optimized for the purposes of
-matching character-based inputs. Specifically, strings are automatically
-expanded into a sequence of character matches (e.g., <? inline_code('"abc"') ?> 
-is matched as <? inline_code('(#\a #\b #\c)') ?>). In order to match values
-that have been protected, one must escape the expression grammar via unquoting
-(i.e., <? inline_code('(unquote "abc")') ?> or <? inline_code(',"abc"') ?>).
+from the input stream generator is "<? inline_code('eqv?') ?>" or
+"<? inline_code('char=?') ?>" to the terminal), however the PEGPEG expression
+grammar has been optimized for the purposes of matching character-based
+inputs. Specifically, strings are automatically expanded into a sequence of
+character matches (e.g., "<? inline_code('"abc"') ?>" is matched as
+"<? inline_code('(#\a #\b #\c)') ?>"). In order to match values that have been
+protected, one must escape the expression grammar via unquoting (i.e.,
+"<? inline_code('(unquote "abc")') ?>" or "<? inline_code(',"abc"') ?>").
 </p>
 
 <div class="section">2.11. Ranges: <? inline_code('(a - b)') ?></div>
 
 <p>
-The <? inline_code('(a - b)') ?> expression will match a range of values,
-inclusively. For example, <? inline_code('(#\0 - #\9)') ?> will match any
+The "<? inline_code('(a - b)') ?>" expression will match a range of values,
+inclusively. For example, "<? inline_code('(#\0 - #\9)') ?>" will match any
 digit character. For consistency, the range expression supports the automatic
 expansion of strings (only for strings of length 1); the expression
-<? inline_code('("0" - "9")') ?> is treated identically to the previous
+"<? inline_code('("0" - "9")') ?>" is treated identically to the previous
 example. Integer ranges can also be matched, to support the use case of
-tokens (e.g., <? inline_code('(0 - 9)') ?>).
+tokens (e.g., "<? inline_code('(0 - 9)') ?>").
 </p>
 
-<div class="section">2.12. Any: <? inline_code('@') ?></div>
+<div class="section">2.12. Any: <? inline_code('%') ?></div>
 
 <p>
-The <? inline_code('@') ?> symbol will match any single value from the
+The "<? inline_code('%') ?>" symbol will match any single value from the
 input stream. In the common case that you want to know what value was matched,
 one can use assignment to capture the input stream that matched. The
-expression <? inline_code('(c <- @)') ?> will match any character and assign
+expression "<? inline_code('(c <- %)') ?>" will match any character and assign
 the list of matching values (of length 1) from the input stream. The
-<? inline_code('@') ?> will not match if there are no more values available
-from the input stream. The expression <? inline_code('(! @)') ?> will only
-"match" when the end of input.
+"<? inline_code('%') ?>" will not match if there are no more values available
+from the input stream. The expression "<? inline_code('(! %)') ?>" will only
+match at the end of input.
 </p>
 
 <div class="section">2.3. Sequence: <? inline_code('(p0 p1 ...)') ?></div>
 
-<p>The <? inline_code('(p0 p1 ...)') ?> expression will match the
+<p>The "<? inline_code('(p0 p1 ...)') ?>" expression will match the
 subexpressions in-order.</p>
 
 <div class="section">2.4. Ordered-choice: <? inline_code('(/ p0 p1 ...)') ?></div>
 
+<p>The "<? inline_code('(/ p0 p1 ...)') ?>" expression will attempt to match only
+one of the the subexpressions by trying each in-order. The expression fails only
+if all of the subexpressions fail.</p>
+
 <div class="section">2.5. Zero-or-More: <? inline_code('(* p0 p1 ...)') ?></div>
 
 <p>
-The <? inline_code('(* p0 p1 ...)') ?> expression will match the sequence
-<? inline_code('(p0 p1 ...)') ?> zero or more times. If there are any
+The "<? inline_code('(* p0 p1 ...)') ?>" expression will match the sequence
+"<? inline_code('(p0 p1 ...)') ?>" zero or more times. If there are any
 variables bound by the subexpressions, then those variables will be bound a
 list of the values for each successful match. For instance,
-<? inline_code('(* some-nt)') ?> will bind <? inline_code('some-nt') ?> to
+"<? inline_code('(* some-nt)') ?>" will bind "<? inline_code('some-nt') ?>" to
 the list of values returned by the nonterminal's semantic actions. In the
-case of zero matches, <? inline_code('some-nt') ?> will be bound to the 
-<? inline_code('peg-unmatched') ?> special object, which can be test for by
-calling the procedure <? inline_code('peg-unmatched?') ?>.
+case of zero matches, "<? inline_code('some-nt') ?>" will be bound to the 
+"<? inline_code('peg-unmatched') ?>" special value, which can be test for by
+calling the procedure "<? inline_code('peg-unmatched?') ?>".
 </p>
 
 <p>
 It's important to note that if there are any ordered-choices that bind
 variables, a bit of indeterminism sneaks in. For instance, expressing "match
 zero or more 'A's and 'B's could be expressed as:
-<? inline_code('(* (/ As Bs))') ?>. However, it is important to remember that
-the variable <? inline_code('As') ?> will be bound to either
-<? inline_code('peg-unmatched') ?> or the list of matches for
-<? inline_code('As') ?>; instances of <? inline_code('peg-unmatched') ?> will
-not be inserted into the list when <? inline_code('Bs') ?> matches. All
-ordering information between the choices of <? inline_code('As') ?> and
-<? inline_code('Bs') ?> will have been lost. In the case that this ordering
+"<? inline_code('(* (/ As Bs))') ?>". However, it is important to remember that
+the variable "<? inline_code('As') ?>" will be bound to either
+"<? inline_code('peg-unmatched') ?>" or the list of matches for
+"<? inline_code('As') ?>"; instances of "<? inline_code('peg-unmatched') ?>"
+will not be inserted into the list when "<? inline_code('Bs') ?>" matches. All
+ordering information between the choices of "<? inline_code('As') ?>" and
+"<? inline_code('Bs') ?>" will have been lost. In the case that this ordering
 information is important, then you should factor out the subexpression as a
 new nonterminal that handles it explicitly.
 </p>
 
 <div class="section">2.6. One-or-More: <? inline_code('(+ p0 p1 ...)') ?></div>
 
+<p>
+The "<? inline_code('(+ p0 p1 ...)') ?>" expression will match the sequence
+"<? inline_code('(p0 p1 ...)') ?>" one or more times. The binding behavior is
+identical to the zero-or-more operator.
+</p>
+
 <div class="section">2.7. Optional: <? inline_code('(? p0 p1 ...)') ?></div>
+
+<p>
+The "<? inline_code('(? p0 p1 ...)') ?>" expression will try to match the
+sequence "<? inline_code('(p0 p1 ...)') ?>" but succeed even if it does not
+match or consume any input.
+</p>
 
 <div class="section">2.8. And-predicate: <? inline_code('(& p0 p1 ...)') ?></div>
 
+<p>
+The "<? inline_code('(& p0 p1 ...)') ?>" expression will try to match the
+sequence "<? inline_code('(p0 p1 ...)') ?>" but will not consume any input.
+The and-predicate can be used as to lookahead in the input stream before
+consuming any input to ensure the sequence matches.
+</p>
+
+<? show_code('(peg-parser
+  [(nl Newline)]
+  (Start
+    [(* (/ nl0 %))
+     (if (peg-unmatched? nl0)
+       0
+       (length nl))])
+  (Newline
+    [((& #\return (! #\newline)) #\return) \'r]
+    [#\return #\newline \'rn]
+    [#\newline \'n]))'); ?>
+
+<p>
+In the example above, the first case in <? inline_code('Newline') ?> 
+("<? inline_code('[((& #\return (! #\newline)) #\return) #t]') ?>") ensures
+that the return character must not be followed by a newline character before
+consuming the return character. Although we have looked at the newline
+character, it will not be consumed.
+</p>
+
 <div class="section">2.9. Not-predicate: <? inline_code('(! p0 p1 ...)') ?></div>
+
+<p>
+The "<? inline_code('(! p0 p1 ...)') ?>" expression will try to match the
+sequence "<? inline_code('(p0 p1 ...)') ?>" but will not consume any input.
+The not-predicate can be used as to lookahead in the input stream before
+consuming any input to ensure the sequence does not match.
+</p>
 
 <div class="section">2.10. Assignment: <? inline_code('(s <- p0 p1 ...)') ?></div>
 
 <p>
-The expression <? inline_code('(s <- p0 p1 ...)') ?> binds the sequence of
+The expression "<? inline_code('(s <- p0 p1 ...)') ?>" binds the sequence of
 values from the input stream that match the expression
-<? inline_code('(p0 p1 ...)') ?> to <? inline_code('s') ?>. For instance, the
-expression <? inline_code('(foobar <- (/ "foo" "bar")') ?> will either bind
-<? inline_code('foobar') ?> to <? inline_code('\'(#\f #\o #\o)') ?> or 
-<? inline_code('\'(#\b #\a #\r)') ?>. If the subexpression consumes no input,
+"<? inline_code('(p0 p1 ...)') ?>" to "<? inline_code('s') ?>". For instance, the
+expression "<? inline_code('(foobar <- (/ "foo" "bar")') ?>" will either bind
+"<? inline_code('foobar') ?>" to "<? inline_code('\'(#\f #\o #\o)') ?>" or
+"<? inline_code('\'(#\b #\a #\r)') ?>". If the subexpression consumes no input,
 then the identifier will be bound to an empty list.
 </p>
 
 <p>
 A common use-case for explicit assignment is for binding the match in a
 character range or "any character" match. In other words, the expression
-<? inline_code('(c <- (/ ("a" - "z") ("A" - "Z")))') ?> matches any alphabetic
+"<? inline_code('(c <- (/ ("a" - "z") ("A" - "Z")))') ?>" matches any alphabetic
 character and the variable <? inline_code('c') ?> takes the list of matching
 characters for the subexpression, a list of one character.
 </p>
@@ -188,7 +265,7 @@ characters for the subexpression, a list of one character.
 <div class="section">5. PEG Debugging</div>
 
 In order to debug your parser, you should call
-<? inline_code('(peg-trace #t)') ?> before the definition of your parser.
+"<? inline_code('(peg-trace #t)') ?>" before the definition of your parser.
 The enabling of tracing affects the code generated for your parser. With
 tracing disabled, the code is simpler and faster. With tracing enabled, you
 will be shown an extremely verbose trace of the matching successes and
